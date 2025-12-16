16ed9fc2ab6a6ef5546889d82096581228e0eb7d
Abidan Brito <43681148+abidanBrito@users.noreply.github.com>
2025-12-16 22:51:02 +0000

    feat: modified buffer face for buffer name

--- sleek-modeline-helpers.el
+++ sleek-modeline-helpers.el
@@ -20,18 +20,30 @@ Requires `nerd-icons' package to be installed."
   :type 'boolean
   :group 'sleek-modeline)
 
+(defcustom sleek-modeline-highlight-modified-buffer-name t
+  "Whether to highlight the buffer name when it has unsaved changes.
+When non-nil, modified buffers will use `sleek-modeline-buffer-name-modified-face'."
+  :type 'boolean
+  :group 'sleek-modeline)
+
 (defun sleek-modeline-buffer-name ()
-  "Show buffer name with custom face and icon (if available)."
+  "Show buffer name with custom face and icon (if available).
+Changes color when buffer is modified."
   (let* ((file-name (buffer-file-name))
          (icon (when (and sleek-modeline-show-icons
                           file-name
                           (featurep 'nerd-icons))
                  (nerd-icons-icon-for-file file-name)))
          (buffer-name (file-name-sans-extension
-                       (substring-no-properties (format-mode-line "%b")))))
+                       (substring-no-properties (format-mode-line "%b"))))
+         ;; Use modified face if buffer has unsaved changes
+         (face (if (and sleek-modeline-highlight-modified-buffer-name
+                        (buffer-modified-p))
+                   'sleek-modeline-buffer-name-modified-face
+                 'sleek-modeline-buffer-name-face)))
     (if icon
-        (concat icon " " (propertize buffer-name 'face 'sleek-modeline-buffer-name-face))
-      (propertize buffer-name 'face 'sleek-modeline-buffer-name-face))))
+        (concat icon " " (propertize buffer-name 'face face))
+      (propertize buffer-name 'face face))))
 
 (defun sleek-modeline-major-mode ()
   "Show major mode with custom face."
